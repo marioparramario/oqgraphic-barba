e6d16f8a7d902556891a4b9dd16a6cbe7792b4b6
Mario Parra <hello@oqgraphic.com>
2018-07-16 02:29:17 +0200

    Adding parallax

--- js/Home.js
+++ js/Home.js
@@ -2,7 +2,15 @@ import Barba from 'barba.js';
 
 const Home = Barba.BaseView.extend({
     namespace: 'home',
-    onEnter: function () {},
+    onEnter: function () {
+    const parent = document.getElementById('container');
+    const parallax = new Parallax(parent, {
+        invertX: true,
+        invertY: true,
+        limitX: 26,
+        limitY: 5,
+    });  
+    },
     onEnterCompleted: function () {},
     onLeave: function () {},
     onLeaveCompleted: function () {}
